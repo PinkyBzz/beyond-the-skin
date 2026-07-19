864079557890a3d949d11dbad9b8ddea4773df30
PinkyBzz <rakhamuhammad191@gmail.com>
2026-07-19 22:50:32 +0700

    Add wave dividers to all sections

--- components/home/changemaker-preview.tsx
+++ components/home/changemaker-preview.tsx
@@ -43,8 +43,17 @@ export async function ChangemakerPreview() {
   return (
     <section
       aria-labelledby="changemaker-heading"
-      className="bg-white py-20 lg:py-28"
+      className="bg-white py-20 lg:py-28 overflow-hidden relative"
     >
+      {/* Top Wave Divider */}
+      <div className="absolute top-0 left-0 w-full overflow-hidden leading-[0]">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" className="w-full h-20 md:h-28 lg:h-36">
+          <path 
+            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" 
+            fill="#FDF9F3"
+          />
+        </svg>
+      </div>
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="mx-auto max-w-4xl">
           <div className="text-center mb-10">
--- components/home/latest-stories.tsx
+++ components/home/latest-stories.tsx
@@ -91,7 +91,16 @@ export async function LatestStories() {
   const stories = await getLatestStories()
 
   return (
-    <section aria-labelledby="latest-stories-heading" className="bg-white py-20 lg:py-28">
+    <section aria-labelledby="latest-stories-heading" className="bg-white py-20 lg:py-28 overflow-hidden relative">
+      {/* Top Wave Divider */}
+      <div className="absolute top-0 left-0 w-full overflow-hidden leading-[0]">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" className="w-full h-20 md:h-28 lg:h-36">
+          <path 
+            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" 
+            fill="#FFE9F1"
+          />
+        </svg>
+      </div>
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between mb-10">
           <div>
